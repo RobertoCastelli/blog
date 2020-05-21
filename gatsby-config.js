56186a55d9507faa130ee1642b97f5cc44854a88
feticcio <feticcio@pop-os.localdomain>
2020-05-21 20:09:00 +0200

    added G mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,21 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby--plugin-mdx`,
+      options: {
+        extentions: [`md`, `.mdx`],
+        gatsbyRemarkplugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -12,6 +28,13 @@ module.exports = {
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
   ],
